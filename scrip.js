7e09eaca8222a1ade8127001f29347ab64cff848
nambvarun <varun@lawn-143-215-53-69.lawn.gatech.edu>
2017-02-22 16:48:20 -0500

    Major Overhaul
    Overhauled the ugly website for something with a little bit more color.
    Also added my hack a thing project. Gotta add more stuff.

---
+++ scrip.js
@@ -0,0 +1,12 @@
+// ;(function()	{
+//     var c = document.getElementByClassName('nav_name');
+
+//     function addAnim() {
+//         c.classList.add('animated')
+//         // remove the listener, no longer needed
+//         c.removeEventListener('mouseover', addAnim);
+//     };
+
+//     // listen to mouseover for the container
+//     c.addEventListener('mouseover', addAnim);
+// })();
